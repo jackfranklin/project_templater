d53377c2cf109831f0d338c69147d773930ca593
Jack Franklin <jack@jackfranklin.net>
2012-12-30 16:39:02 +0000

    make generator make_file method take a block for file contents

--- lib/generators/generator.rb
+++ lib/generators/generator.rb
@@ -3,9 +3,14 @@ require "fileutils"
 class Generator
   def initialize(base_dir)
     @base = base_dir
+    unless @base[-1, 1] == "/"
+      @base = @base + "/"
+    end
   end
 
-  def make_file(path, contents = "")
+  def make_file(path, &block)
+    contents = ""
+    contents = block.call if block_given?
     File.open(@base + path, 'w') {|f| f.write(contents) }
   end
 
