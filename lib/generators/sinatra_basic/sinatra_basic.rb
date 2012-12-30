c942590b422aafe5a2307839f92297d7fc781dd6
Jack Franklin <jack@jackfranklin.net>
2012-12-30 16:46:40 +0000

    flush out sinatra_basic

--- lib/generators/sinatra_basic/sinatra_basic.rb
+++ lib/generators/sinatra_basic/sinatra_basic.rb
@@ -2,7 +2,19 @@ require_relative "../../generators/generator.rb"
 
 class SinatraBasic < Generator
   def run
-    make_file("app.rb")
-    make_file("config.ru", "require './app'\nrun Sinatra::Application")
+    make_file("Gemfile") {
+      "source 'http://rubygems.org'\ngem 'sinatra'"
+    }
+    make_file("app.rb") {
+      "require 'sinatra'"
+    }
+    make_file("config.ru") {
+      "require './app'\nrun Sinatra::Application"
+    }
+    make_dir("public")
+    make_dir("public/css")
+    make_dir("public/js")
+    make_file("public/css/style.css")
+    make_file("public/js/app.js")
   end
 end
--- lib/project_templater.rb
+++ lib/project_templater.rb
@@ -16,7 +16,7 @@ class ProjectTemplater
   end
 
   def run(base_dir)
-    base_dir = Dir.pwd + "/" + base_dir
+    base_dir = Dir.pwd + "/" + (base_dir ||= "")
     class_name = @template.camel_case
     instance = Object::const_get(class_name).new(base_dir)
     instance.run
